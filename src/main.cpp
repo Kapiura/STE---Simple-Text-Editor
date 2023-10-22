fcc53ea3133060a0f4031885ff957ea170c4dde7
Kapiura <swinxten@gmail.com>
2023-10-22 13:07:20 +0200

    Created and added to projects src, lib directories and tags file
    - Added SDL library to lib dir
    - Small changes for src/main.cpp file to see if simple build works
    - Added tags, idk where is it from, but it is important i guess

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
+#include <SDL2/SDL_error.h>
 #include <iostream>
+#include <SDL2/SDL.h>
 
 int main()
 {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cout << "SDL could not initialize! SDL_Error:" << SDL_GetError() << "\n";
+		return 1;
+	}
+    std::cout << "Testowy main.cpp\n";
   return 0;
 }
