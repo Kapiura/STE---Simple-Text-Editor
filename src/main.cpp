da4f95ff96527e686239c43376fbf01e9f6c94bf
Kapiura <swinxten@gmail.com>
2023-10-23 08:13:17 +0200

    Update main.cpp file
    - Small update of main.cpp file
    - Avaible to build it and run
    - Shows only window uwu

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,39 @@
 #include <SDL.h>
 
-class SDLApplication {
-public:
-    SDLApplication() {
-        if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-            SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
-            // You might want to handle the initialization failure here.
-        }
-
-        window = SDL_CreateWindow(
-            "SDL Window",
-            SDL_WINDOWPOS_CENTERED,
-            SDL_WINDOWPOS_CENTERED,
-            800,
-            600,
-            SDL_WINDOW_SHOWN
-        );
-
-        if (!window) {
-            SDL_Log("Failed to create window: %s", SDL_GetError());
-            // You might want to handle the window creation failure here.
-        }
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
     }
 
-    ~SDLApplication() {
-        SDL_DestroyWindow(window);
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL Window",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (!window) {
+        SDL_Log("Failed to create window: %s", SDL_GetError());
         SDL_Quit();
+        return 1;
     }
 
-    void Run() {
-        bool quit = false;
-        SDL_Event event;
-
-        while (!quit) {
-            while (SDL_PollEvent(&event)) {
-                if (event.type == SDL_QUIT) {
-                    quit = true;
-                }
+    // Wait for the user to close the window
+    bool quit = false;
+    SDL_Event event;
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
             }
         }
     }
 
-private:
-    SDL_Window* window;
-};
-
-int main() {
-    SDLApplication app;
-    app.Run();
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
 
