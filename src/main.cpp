ef02cfe4be50b358f6ad20d607e2f0d0099f225d
Kapiura <swinxten@gmail.com>
2023-10-23 08:05:37 +0200

    Update main.cpp
    - Create class Application that allow user to open window

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,53 @@
-#include <SDL2/SDL_error.h>
-#include <iostream>
-#include <SDL2/SDL.h>
+#include <SDL.h>
 
-int main()
-{
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		std::cout << "SDL could not initialize! SDL_Error:" << SDL_GetError() << "\n";
-		return 1;
-	}
-    std::cout << "Testowy main.cpp\n";
-  return 0;
+class SDLApplication {
+public:
+    SDLApplication() {
+        if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+            SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+            // You might want to handle the initialization failure here.
+        }
+
+        window = SDL_CreateWindow(
+            "SDL Window",
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            800,
+            600,
+            SDL_WINDOW_SHOWN
+        );
+
+        if (!window) {
+            SDL_Log("Failed to create window: %s", SDL_GetError());
+            // You might want to handle the window creation failure here.
+        }
+    }
+
+    ~SDLApplication() {
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
+
+    void Run() {
+        bool quit = false;
+        SDL_Event event;
+
+        while (!quit) {
+            while (SDL_PollEvent(&event)) {
+                if (event.type == SDL_QUIT) {
+                    quit = true;
+                }
+            }
+        }
+    }
+
+private:
+    SDL_Window* window;
+};
+
+int main() {
+    SDLApplication app;
+    app.Run();
+    return 0;
 }
+
