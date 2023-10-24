f87d500d679aa38382cbd62d12832b8a1dab5042
Kapiura <swinxten@gmail.com>
2023-10-24 11:12:14 +0200

    Update main.cpp and .gitignore files
    - Change including SDL library Line
    - Add to ignorign formatting nvim file

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,39 @@
-#include <SDL.h>
+#include <SDL2/SDL.h>
 
-int main() {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
-        return 1;
+int
+main ()
+{
+  if (SDL_Init (SDL_INIT_VIDEO) != 0)
+    {
+      SDL_Log ("Unable to initialize SDL: %s", SDL_GetError ());
+      return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow(
-        "STE - SimpleTextEditor",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
-        SDL_WINDOW_SHOWN
-    );
+  SDL_Window *window
+      = SDL_CreateWindow ("STE - SimpleTextEditor", SDL_WINDOWPOS_CENTERED,
+                          SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
 
-    if (!window) {
-        SDL_Log("Failed to create window: %s", SDL_GetError());
-        SDL_Quit();
-        return 1;
+  if (!window)
+    {
+      SDL_Log ("Failed to create window: %s", SDL_GetError ());
+      SDL_Quit ();
+      return 1;
     }
 
-    // Wait for the user to close the window
-    bool quit = false;
-    SDL_Event event;
-    while (!quit) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                quit = true;
+  bool quit = false;
+  SDL_Event event;
+  while (!quit)
+    {
+      while (SDL_PollEvent (&event))
+        {
+          if (event.type == SDL_QUIT)
+            {
+              quit = true;
             }
         }
     }
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
+  SDL_DestroyWindow (window);
+  SDL_Quit ();
+  return 0;
 }
-
