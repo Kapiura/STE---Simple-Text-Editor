e337e80afeba0154f8b6375e525b908280193853
Kapiura <swinxten@gmail.com>
2023-10-24 11:55:34 +0200

    Update main.cpp file
    - Added class Editor
    - Added method run() for Editor class
    - Avaible to create and run simple 600x800 window
    - Avaible to exit programm by clicking on red button/ x

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,67 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_error.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_video.h>
+#include <iostream>
+
+class Editor
+{
+public:
+  Editor (const char *title, int width, int height)
+      : title (title), width (width), height (height)
+  {
+    if (SDL_Init (SDL_INIT_VIDEO) < 0)
+      {
+        std::cout << "Init SDL error: " << SDL_GetError () << "\n";
+        exit (-1);
+      }
+
+    window = SDL_CreateWindow (title, SDL_WINDOWPOS_CENTERED,
+                               SDL_WINDOWPOS_CENTERED, width, height,
+                               SDL_WINDOW_SHOWN);
+    if (!window)
+      {
+        std::cout << "Error during creating window: " << SDL_GetError ()
+                  << "\n";
+        exit (-1);
+      }
+  }
+
+  ~Editor ()
+  {
+    SDL_DestroyWindow (window);
+    SDL_Quit ();
+  }
+
+  void
+  run ()
+  {
+    bool quit = false;
+    SDL_Event event;
+
+    while (!quit)
+      {
+        while (SDL_PollEvent (&event) != 0)
+          {
+            if (event.type == SDL_QUIT)
+              {
+                quit = true;
+              }
+          }
+        SDL_Delay (16);
+      }
+  }
+
+private:
+  const char *title;
+  int width;
+  int height;
+  SDL_Window *window;
+};
 
 int
 main ()
 {
-  if (SDL_Init (SDL_INIT_VIDEO) != 0)
-    {
-      SDL_Log ("Unable to initialize SDL: %s", SDL_GetError ());
-      return 1;
-    }
-
-  SDL_Window *window
-      = SDL_CreateWindow ("STE - SimpleTextEditor", SDL_WINDOWPOS_CENTERED,
-                          SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-
-  if (!window)
-    {
-      SDL_Log ("Failed to create window: %s", SDL_GetError ());
-      SDL_Quit ();
-      return 1;
-    }
-
-  bool quit = false;
-  SDL_Event event;
-  while (!quit)
-    {
-      while (SDL_PollEvent (&event))
-        {
-          if (event.type == SDL_QUIT)
-            {
-              quit = true;
-            }
-        }
-    }
-
-  SDL_DestroyWindow (window);
-  SDL_Quit ();
-  return 0;
+  Editor editor ("STE - Simple Text Editor", 600, 800);
+  editor.run ();
 }
