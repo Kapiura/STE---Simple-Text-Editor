295d0ce601cc6177892782f116c837646b477304
kapiura <swinxten@gmail.com>
2023-12-02 12:27:28 +0100

    Rename TextEditor class to InputEditor
    - renamed class and files linked to it
    - modify rendering method of text

---
+++ include/InputEditor.h
@@ -0,0 +1,50 @@
+#include "EditorWindow.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_ttf.h>
+#include <string>
+#include <vector>
+#pragma once
+#ifndef TEXTEDITOR_H
+#define TEXTEDITOR_H
+
+class InputEditor {
+public:
+  InputEditor(SDL_Renderer *renderer, const EditorWindow &editorWindow);
+  ~InputEditor();
+
+  void handleEvents(SDL_Event &e);
+  void update();
+  void render();
+
+  void setText(const std::string &text);
+
+  std::string getTextContent();
+
+protected:
+  void renderCursor();
+  void renderTextArea();
+
+  void checkVisibleCursor();
+  void changeCursorTimer();
+
+  void loadFont(const std::string &fontPath, int fontSize);
+
+  SDL_Renderer *_renderer;
+  // SDL_Surface *_surface;
+  SDL_Rect _textArea;
+  TTF_Font *_font;
+  EditorWindow _editorWindow;
+  std::string *_fontPath;
+
+private:
+  std::vector<std::string> _textInput;
+  int fontSize;
+  int cursorOnCurrentLine;
+  int cursonOnCurrentChar;
+
+  bool _cursorVisible;
+};
+
+#endif // TEXTEDITOR_H
