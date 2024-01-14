523d8468b5cbce0e9d169cb08d29197cc2ce47d6
kapiura <swinxten@gmail.com>
2024-01-14 15:32:37 +0100

    Added CustomizationApp class
    - added new class
    - still not using
    - created simple constructor and destrucor of class
    - just simple scheme of class

---
+++ include/CustomizationApp.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef CUSTOMIZATIONAPP_H
+#define CUSTOMIZATIONAPP_H
+
+class CustomizationApp {
+public:
+  CustomizationApp();
+  ~CustomizationApp();
+};
+
+#endif // CUSTOMIZATIONAPP_H
