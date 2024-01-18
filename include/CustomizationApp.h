c3fa546c3166d2a564212708adbb460e643386c6
kapiura <swinxten@gmail.com>
2024-01-18 15:42:56 +0100

    Update CustomizationApp class - added method that render values background color in rgb - added method that render all customization window

--- include/CustomizationApp.h
+++ include/CustomizationApp.h
@@ -1,24 +1,31 @@
 #pragma once
 #include "EditorWindow.h"
+#include "InputEditor.h"
 #include "Slider.h"
 #ifndef CUSTOMIZATIONAPP_H
 #define CUSTOMIZATIONAPP_H
 
 class CustomizationApp {
 public:
-  CustomizationApp(EditorWindow *edit);
+  CustomizationApp(EditorWindow *edit, InputEditor *in);
   ~CustomizationApp();
 
-  void render();
+  void render(int mouseX, int mouseY);
+
+  void changeSliderValue(int mouseX, int mouseY);
 
 private:
   EditorWindow *_editorWindow;
+  InputEditor *_editorInput;
   int _width;
   int _height;
   TTF_Font *_font;
   Slider *sliderRed;
   Slider *sliderGreen;
   Slider *sliderBlue;
+  SDL_Rect btnSetDefault;
+  SDL_Rect btnSetCurrent;
+  SDL_Color backgroundColor = {198, 206, 206, 255};
 };
 
 #endif // CUSTOMIZATIONAPP_H
--- src/CustomizationApp.cpp
+++ src/CustomizationApp.cpp
@@ -2,16 +2,48 @@
 #include "../include/Slider.h"
 #include <iostream>
 
-CustomizationApp::CustomizationApp(EditorWindow *edit) : _editorWindow(edit) {
-  sliderRed = new Slider(_editorWindow->getFont());
-  sliderGreen = new Slider(_editorWindow->getFont());
-  sliderBlue = new Slider(_editorWindow->getFont());
+CustomizationApp::CustomizationApp(EditorWindow *edit, InputEditor *in)
+    : _editorWindow(edit), _editorInput(in) {
+  // window, font, x, w, y, h
+  sliderRed = new Slider(_editorWindow, _editorInput, 10, 10, 10, 265);
+  sliderGreen = new Slider(_editorWindow, _editorInput, 80, 10, 10, 265);
+  sliderBlue = new Slider(_editorWindow, _editorInput, 150, 10, 10, 265);
+  sliderRed->setSliderColor(255, 0, 0);
+  sliderGreen->setSliderColor(0, 255, 0);
+  sliderBlue->setSliderColor(0, 0, 255);
+
   std::cout << "CustomizationApp object has been created\n";
 }
 CustomizationApp::~CustomizationApp() {
+  delete sliderRed;
+  delete sliderGreen;
+  delete sliderBlue;
   std::cout << "CustomizationApp object has been deleted\n";
 }
 
-void CustomizationApp::render() {
-  SDL_SetRenderDrawColor(_editorWindow->getRenderer(), 200, 200, 200, 255);
+void CustomizationApp::render(int mouseX, int mouseY) {
+  SDL_RenderClear(_editorWindow->getRenderer());
+  SDL_SetRenderDrawColor(_editorWindow->getRenderer(), backgroundColor.r,
+                         backgroundColor.g, backgroundColor.b,
+                         backgroundColor.a);
+
+  sliderRed->renderSlider();
+  sliderGreen->renderSlider();
+  sliderBlue->renderSlider();
+  sliderRed->renderSliderMove(mouseX, mouseY);
+  sliderGreen->renderSliderMove(mouseX, mouseY);
+  sliderBlue->renderSliderMove(mouseX, mouseY);
+  sliderRed->renderValueText();
+  sliderGreen->renderValueText();
+  sliderBlue->renderValueText();
+  SDL_SetRenderDrawColor(_editorWindow->getRenderer(), backgroundColor.r,
+                         backgroundColor.g, backgroundColor.b,
+                         backgroundColor.a);
+}
+
+void CustomizationApp::changeSliderValue(int mouseX, int mouseY) {
+  render(mouseX, mouseY);
+  sliderRed->setSliderValue(mouseX, mouseY);
+  sliderBlue->setSliderValue(mouseX, mouseY);
+  sliderGreen->setSliderValue(mouseX, mouseY);
 }
